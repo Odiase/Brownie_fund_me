8d26aea31ca693cf9f8dcad1861e30abef5c5fbf
Odiase <odiaseefosa18@gmail.com>
2022-12-14 14:06:43 +0100

    added my local ganache network to the list of networks on brownie

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,13 +3,19 @@ from brownie import accounts, config, FundMe, MockV3Aggregator, network
 from web3 import Web3
 
 # local imports
-from .extras import get_account, deploy_mocks
+from .extras import get_account, deploy_mocks, LOCAL_BLOCKCHIAN_ENVIRONMENTS
 
 
 def deploy_fund_me():
     account = get_account()
-    publish_source = config['networks'][network.show_active()].get('verify')
-    price_feed_address = deploy_mocks()
+
+    if network.show_active() not in LOCAL_BLOCKCHIAN_ENVIRONMENTS:
+        price_feed_address = config['networks'][network.show_active(
+        )]['eth_usd_price_feed']
+        publish_source = config['networks'][network.show_active()].get('verify')
+    else:
+        price_feed_address = deploy_mocks()
+        publish_source = False
 
     # deploying fund me contract
     fund_me = FundMe.deploy(
--- scripts/extras.py
+++ scripts/extras.py
@@ -3,10 +3,14 @@ from brownie import accounts, config, network, MockV3Aggregator
 from web3 import Web3
 
 
+DECIMALS = 18
+STARTING_PRICE = 2000
+LOCAL_BLOCKCHIAN_ENVIRONMENTS = ["development", "ganache-local"]
+
 def get_account():
     '''Getting Account To Work With.'''
 
-    if network.show_active() == "development":
+    if network.show_active() in LOCAL_BLOCKCHIAN_ENVIRONMENTS:
         return accounts[0]
         # getting my connected blockchain network
     return accounts.add(config['wallets']['private_key'])
@@ -16,15 +20,11 @@ def deploy_mocks():
     account = get_account()
     # if we are on a persistent network(goerli...etc), use associated price_feed address,
     #  else use mocks
-    if network.show_active() != "development":
-        price_feed_address = config['networks'][network.show_active(
-        )]['eth_usd_price_feed']
-    else:
-        if len(MockV3Aggregator) <= 0:
-            mock_aggregator = MockV3Aggregator.deploy(
-                18, Web3.toWei(2000, 'ether'), 
-                {"from": account}
-            )
+    if len(MockV3Aggregator) <= 0:
+        mock_aggregator = MockV3Aggregator.deploy(
+            DECIMALS, Web3.toWei(STARTING_PRICE, 'ether'), 
+            {"from": account}
+        )
         
     price_feed_address = MockV3Aggregator[-1].address
 
