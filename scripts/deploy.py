5fe0ac3f025f6f7d67744078eda485edaf1fecf3
Odiase <odiaseefosa18@gmail.com>
2022-12-14 11:39:16 +0100

    remove redundant code

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -7,21 +7,24 @@ from .extras import get_account
 
 def deploy_fund_me():
     account = get_account()
-
+    publish_source = config['networks'][network.show_active()].get('verify')
+    
     # if we are on a persistent network(goerli...etc), use associated price_feed address,
     #  else use mocks
     if network.show_active() != "development":
-        price_feed_address = config['networks'][network.show_active()]['eth_usd_price_feed']
+        price_feed_address = config['networks'][network.show_active(
+        )]['eth_usd_price_feed']
     else:
-        mock_aggregator = MockV3Aggregator.deploy(18, 2000000000000000000000, {"from" : account})
+        mock_aggregator = MockV3Aggregator.deploy(
+            18, 2000000000000000000000, 
+            {"from": account}
+        )
         price_feed_address = mock_aggregator.address
-        publish_source = False
-    
+
     # deploying fund me contract
-    publish_source = config['networks'][network.show_active()].get('verify')
     fund_me = FundMe.deploy(
         price_feed_address,
-        {"from": account}, 
+        {"from": account},
         publish_source=publish_source
     )
 
